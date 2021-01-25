536c3e3b00608f012b67e2d4ef3e21dc3326ad7d
Ahmad Muzakki <yitna.yuana@gmail.com>
2021-01-25 12:24:13 +0700

    add aff.js

--- affiliate/affiliate.js
+++ affiliate/affiliate.js
@@ -1,16 +1,16 @@
 // Data Affiliate Link & Images -----
 var dataAffiliate320 = [
-	{"id":1, "url":"https://niagahoster.amz", "image":"assets/images/ex-img-1.png"},
-	{"id":2, "url":"https://mobilbaru.amz", "image":"assets/images/ex-img-2.png"},
-	{"id":3, "url":"https://talend.amz", "image":"assets/images/ex-img-3.png"},
-	{"id":4, "url":"https://w3schools.amz", "image":"assets/images/ex-img-4.png"},
-	{"id":5, "url":"https://newchic.amz", "image":"assets/images/ex-img-5.gif"},
-	{"id":6, "url":"https://detik.amz", "image":"assets/images/ex-img-6.jpg"},
-	{"id":7, "url":"https://tokoped.amz", "image":"assets/images/ex-img-7.jpg"},
-	{"id":8, "url":"https://panasonic.amz", "image":"assets/images/ex-img-8.png"},
-	{"id":9, "url":"https://olx.amz", "image":"assets/images/ex-img-9.png"},
-	{"id":10, "url":"https://nanboya.amz", "image":"assets/images/ex-img-10.png"},
-	{"id":11, "url":"https://leminerale.amz", "image":"assets/images/ex-img-11.png"},
+	{"id":1, "url":"https://niagahoster.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-1.png"},
+	{"id":2, "url":"https://mobilbaru.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-2.png"},
+	{"id":3, "url":"https://talend.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-3.png"},
+	{"id":4, "url":"https://w3schools.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-4.png"},
+	{"id":5, "url":"https://newchic.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-5.gif"},
+	{"id":6, "url":"https://detik.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-6.jpg"},
+	{"id":7, "url":"https://tokoped.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-7.jpg"},
+	{"id":8, "url":"https://panasonic.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-8.png"},
+	{"id":9, "url":"https://olx.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-9.png"},
+	{"id":10, "url":"https://nanboya.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-10.png"},
+	{"id":11, "url":"https://leminerale.amz", "image":"http://cdn.kukode.in/affiliate/images/ex-img-11.png"},
 ];
 
 // Display Randomly Affiliate by ID -----
