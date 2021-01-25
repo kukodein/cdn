81f511b49c8ce8e045e98ec097115d2ac4192d87
Ahmad Muzakki <yitna.yuana@gmail.com>
2021-01-25 12:21:56 +0700

    add aff.js

---
+++ affiliate/affiliate.js
@@ -0,0 +1,45 @@
+// Data Affiliate Link & Images -----
+var dataAffiliate320 = [
+	{"id":1, "url":"https://niagahoster.amz", "image":"assets/images/ex-img-1.png"},
+	{"id":2, "url":"https://mobilbaru.amz", "image":"assets/images/ex-img-2.png"},
+	{"id":3, "url":"https://talend.amz", "image":"assets/images/ex-img-3.png"},
+	{"id":4, "url":"https://w3schools.amz", "image":"assets/images/ex-img-4.png"},
+	{"id":5, "url":"https://newchic.amz", "image":"assets/images/ex-img-5.gif"},
+	{"id":6, "url":"https://detik.amz", "image":"assets/images/ex-img-6.jpg"},
+	{"id":7, "url":"https://tokoped.amz", "image":"assets/images/ex-img-7.jpg"},
+	{"id":8, "url":"https://panasonic.amz", "image":"assets/images/ex-img-8.png"},
+	{"id":9, "url":"https://olx.amz", "image":"assets/images/ex-img-9.png"},
+	{"id":10, "url":"https://nanboya.amz", "image":"assets/images/ex-img-10.png"},
+	{"id":11, "url":"https://leminerale.amz", "image":"assets/images/ex-img-11.png"},
+];
+
+// Display Randomly Affiliate by ID -----
+function displayRandomImage(data) {
+	// Get random ID -----
+	var randomID = Math.floor((Math.random() * data.length) + 1);
+	var item = data.find(item => item.id === randomID);
+	// Set Attribute -----
+	document.getElementById("ban-link").setAttribute("href", item.url);
+	document.getElementById("ban-img").src = item.image;
+}
+
+// Change Affiliate by Interval time -----
+function startTimer(data) {
+	document.getElementById("ban-link").setAttribute("href", dataAffiliate320[0].url);
+	document.getElementById("ban-img").src = dataAffiliate320[0].image;
+	
+	setInterval( function() { 
+		displayRandomImage(data); 
+	},5000);
+}
+
+/* 	Example run the Function -----
+
+js -----
+startTimer(dataAffiliate320);
+
+html -----
+<a id="ban-link" target="_blank"><img id="ban-img"></img></a>
+<script type="text/javascript" src="affiliate.js"></script>
+<script type="text/javascript">startTimer(dataAffiliate320);</script>
+*/
