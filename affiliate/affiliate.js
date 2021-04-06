de6a22c1fc73de415e0ed3439fd1115e846baacc
Ahmad Muzakki <yitna.yuana@gmail.com>
2021-04-06 14:54:48 +0700

    commit

--- affiliate/affiliate.js
+++ affiliate/affiliate.js
@@ -1,16 +1,16 @@
 // Data Affiliate Link & Images -----
 var dataAffiliate320 = [
-	{"id":1, "url":"https://niagahoster.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-1.png"},
-	{"id":2, "url":"https://mobilbaru.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-2.png"},
-	{"id":3, "url":"https://talend.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-3.png"},
-	{"id":4, "url":"https://w3schools.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-4.png"},
-	{"id":5, "url":"https://newchic.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-5.gif"},
-	{"id":6, "url":"https://detik.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-6.jpg"},
-	{"id":7, "url":"https://tokoped.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-7.jpg"},
-	{"id":8, "url":"https://panasonic.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-8.png"},
-	{"id":9, "url":"https://olx.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-9.png"},
-	{"id":10, "url":"https://nanboya.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-10.png"},
-	{"id":11, "url":"https://leminerale.amz", "image":"https://kukode.in/cdn/affiliate/images/ex-img-11.png"},
+	{"id":1, "url":"https://niagahoster.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-1.png"},
+	{"id":2, "url":"https://mobilbaru.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-2.png"},
+	{"id":3, "url":"https://talend.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-3.png"},
+	{"id":4, "url":"https://w3schools.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-4.png"},
+	{"id":5, "url":"https://newchic.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-5.gif"},
+	{"id":6, "url":"https://detik.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-6.jpg"},
+	{"id":7, "url":"https://tokoped.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-7.jpg"},
+	{"id":8, "url":"https://panasonic.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-8.png"},
+	{"id":9, "url":"https://olx.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-9.png"},
+	{"id":10, "url":"https://nanboya.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-10.png"},
+	{"id":11, "url":"https://leminerale.amz", "image":"https://cdn.kukode.in/affiliate/images/ex-img-11.png"},
 ];
 
 // Display Randomly Affiliate by ID -----
@@ -40,6 +40,6 @@ startTimer(dataAffiliate320);
 
 html -----
 <a id="ban-link" target="_blank"><img id="ban-img"></img></a>
-<script type="text/javascript" src="https://kukode.in/cdn/affiliate/affiliate.js"></script>
+<script type="text/javascript" src="https://cdn.kukode.in/affiliate/affiliate.js"></script>
 <script type="text/javascript">startTimer(dataAffiliate320);</script>
 */
