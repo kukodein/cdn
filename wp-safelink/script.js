65ff454b6390780096f57b9921706055ea9feb94
Ahmad Muzakki <yitna.yuana@gmail.com>
2021-07-05 16:02:40 +0700

    commit

--- wp-safelink/script.js
+++ wp-safelink/script.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "https://wacanan.com/script/go?url=" + encryptedUrl;
+a_to_ve[i].href = "https://wacanan.com/nulled/go?url=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
